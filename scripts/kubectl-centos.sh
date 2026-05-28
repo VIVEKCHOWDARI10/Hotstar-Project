78f61bc67866412404de37b276083eb7452fbdc1
VIVEKCHOWDARI10 <vivekchowdari10@gmail.com>
2026-05-28 14:06:21 +0530

    Update kubectl-centos.sh

--- scripts/kubectl-centos.sh
+++ scripts/kubectl-centos.sh
@@ -1,1 +1,19 @@
-//
+#!/bin/bash
+
+# Update system
+sudo yum update -y
+
+# Install curl
+sudo yum install curl -y
+
+# Download latest kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+
+# Give execute permission
+chmod +x kubectl
+
+# Move kubectl to system path
+sudo mv kubectl /usr/local/bin/
+
+# Verify installation
+kubectl version --client
