c62ae4dae09c3711d9e63f7f1655d855926f22ac
VIVEKCHOWDARI10 <vivekchowdari10@gmail.com>
2026-05-28 13:36:42 +0530

    Update jenkins-centos.sh

--- scripts/jenkins-centos.sh
+++ scripts/jenkins-centos.sh
@@ -1,1 +1,24 @@
-//
+#!/bin/bash
+
+# Update system
+sudo yum update -y
+
+# Install Java 17
+sudo yum install java-17-openjdk -y
+
+# Add Jenkins repo
+sudo wget -O /etc/yum.repos.d/jenkins.repo \
+https://pkg.jenkins.io/redhat-stable/jenkins.repo
+
+# Import Jenkins key
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+
+# Install Jenkins
+sudo yum install jenkins -y
+
+# Enable and start Jenkins
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
+
+# Check status
+sudo systemctl status jenkins
